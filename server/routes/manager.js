ed2462468597922795b1b1035329629dfcea33c7
Raja28 <rajadavid03@gmail.com>
2025-06-06 09:52:52 +0530

    fix: updated updateProject route

--- server/routes/manager.js
+++ server/routes/manager.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { engineerList, addProject } = require("../controllers/manager")
+const { engineerList, addProject, deleteProject, updateProject } = require("../controllers/manager")
 const { auth, requireRole } = require("../middleware/auth")
 const router = express.Router()
 
@@ -8,6 +8,8 @@ const engineer = "Engineer"
 
 router.get("/engineersList", auth, requireRole(manager), engineerList)
 router.post("/addProject", auth, requireRole(manager), addProject)
+router.post("/updateProject", auth, requireRole(manager), updateProject)
+router.delete("/project/:projectId", auth, requireRole(manager), deleteProject)
 
 
 module.exports = router
