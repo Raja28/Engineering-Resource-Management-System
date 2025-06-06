9826e8a63d6d72c5d962a79ec660150c285996cc
Raja28 <rajadavid03@gmail.com>
2025-06-06 09:15:08 +0530

    fix: updated UPDATE_PROJECT_API

--- client/src/utils/apis.ts
+++ client/src/utils/apis.ts
@@ -4,3 +4,5 @@ export const USER_SIGNUP_API = BASE_URL + "/api/auth/signup"
 export const USER_LOGIN_API = BASE_URL + "/api/auth/login"
 
 export const ADD_PROJECT_API = BASE_URL + "/api/manager/addProject"
+export const DELETE_PROJECT_API = BASE_URL + "/api/manager/project"
+export const UPDATE_PROJECT_API = BASE_URL + "/api/manager/updateProject"
