d5bb7d6b220ddf1a8f74116dd54e73fb80bb952e
Raja28 <rajadavid03@gmail.com>
2025-06-06 12:02:01 +0530

    fix: disabled signup button while signingup

--- client/src/components/EngineerSignupForm.tsx
+++ client/src/components/EngineerSignupForm.tsx
@@ -241,7 +241,7 @@ export function EngineerSignupForm({ handleSubmit, register, errors, setShowForm
                 <button type="submit"
                     disabled={status === _status ? true : false}
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-                    Submit
+                    {status === _status ? "Please wait...":"Submit"}
                 </button>
             </div>
         </form>
